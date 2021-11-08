4ff51f740809bfbb392f5fa7ffa8b3deb1367d68
Sam Cate <samcate@Sams-MacBook-Pro.local>
2021-11-07 19:12:24 -0500

    more bug fixes

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@ var app = angular.module('mainApp', ['ui.router'])
 
 app.config(function($stateProvider, $urlRouterProvider) {
 
-  $urlRouterProvider.otherwise('/#!/');
+  $urlRouterProvider.otherwise('/');
 
   $stateProvider
 
