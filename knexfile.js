5faa2ec436001995cb07f932c8010c6f0247c2fe
Sam Cate <samcate@Sams-MBP.hsd1.in.comcast.net>
2023-03-12 01:21:40 -0500

    more hosting attempts

--- knexfile.js
+++ knexfile.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const ca = fs.readFileSync('./db/ca-certificate.crt');
 console.log(process.env.DATABASE_URL);
+console.log('Knex configuration loaded:', process.env.NODE_ENV);
 module.exports = {
 
   development: {
